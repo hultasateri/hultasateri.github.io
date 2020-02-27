59f05a083fc1237bf801806083a2898e6ae84125
Joel Askling <joelaskling@Joels-MBP.lan>
2020-02-27 21:31:58 +0100

    Init

---
+++ precache-manifest.6e4d0822ef1366191be2f48f17d60bce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a36dc627d0dab97ec51874e2a55b956",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b08cff5038875ab1ca40",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3f0864d07bf79b19dc5a",
+    "url": "./static/js/2.cb871d89.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.cb871d89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08cff5038875ab1ca40",
+    "url": "./static/js/main.91e45831.chunk.js"
+  },
+  {
+    "revision": "30342b4621399fc97af5",
+    "url": "./static/js/runtime-main.2bba1f4a.js"
+  },
+  {
+    "revision": "e75a4315abf483c3ea031d56f9df9ea4",
+    "url": "./static/media/hulta.e75a4315.jpg"
+  }
+]);
